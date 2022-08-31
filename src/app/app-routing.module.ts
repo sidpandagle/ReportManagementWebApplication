151af8e53ee48834c777f6189189eb055924c065
sidpandagle <siddhant.pandagle1998@gmail.com>
2022-08-31 22:01:31 +0530

    Add Report Form Started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { ReportComponent } from './report/report.component';
 import { FormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoryAddComponent } from './category-add/category-add.component';
+import { ReportAddComponent } from './report-add/report-add.component';
 
 const routes: Routes = [
   {path: '', component: DashboardComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'category', component: CategoryComponent},
   {path: 'report', component: ReportComponent},
+  {path: 'add-category', component: CategoryAddComponent},
+  {path: 'add-report', component: ReportAddComponent},
   {path: '**', redirectTo: '/dashboard', pathMatch: 'full'},
 ];
 
